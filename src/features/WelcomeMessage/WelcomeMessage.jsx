8e527f9b1dacdbf0ff1e7dc983b579ebb1b2ed5f
irened123 <irenedeng7@gmail.com>
2024-08-12 07:44:28 -0400

    Create WelcomeMessage with text and styling

---
+++ src/features/WelcomeMessage/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './WelcomeMessage.css';
+
+const WelcomeMessage = () => {
+  return (
+    <div className="welcome-container">
+      <h2 className="welcome-heading">Welcome to StudentThreads!</h2>
+      <p className="welcome-content">Your Cozy Corner for College Life :)</p>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
