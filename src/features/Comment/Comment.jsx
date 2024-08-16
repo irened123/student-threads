05a96756d29e6c04ad1150c29911e99a5aa652d2
irened123 <irenedeng7@gmail.com>
2024-08-16 16:41:21 -0400

    Add Comment component with styles for displaying post comments

---
+++ src/features/Comment/Comment.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import moment from 'moment';
+import ReactMarkdown from 'react-markdown';
+import './Comment.css';
+
+const Comment = ({ comment }) => {
+  return (
+    <div className="comment">
+      <div className="comment-metadata">
+        <p className="comment-author">{comment.author}</p>
+        <p className="comment-time">
+          {moment.unix(comment.created_utc).fromNow()}
+        </p>
+      </div>
+      <div className="comment-body">
+        <ReactMarkdown>{comment.body}</ReactMarkdown>
+      </div>
+    </div>
+  );
+};
+
+export default Comment;
