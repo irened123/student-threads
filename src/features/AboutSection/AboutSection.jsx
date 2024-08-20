e861a8fe26d05159bf1b4e1ebe1e4a99440c7edb
irened123 <irenedeng7@gmail.com>
2024-08-19 23:39:44 -0400

    Update font family and color for improved text styling; improve responsiveness

--- src/features/AboutSection/AboutSection.jsx
+++ src/features/AboutSection/AboutSection.jsx
@@ -7,16 +7,16 @@ const AboutSection = () => {
       <h2 className="about-heading">About</h2>
       <div className="about-content">
         <p>StudentThreads is designed to simplify and enrich your college experience. Whether you're looking to boost productivity, share a laugh, or take a peaceful break, we've gathered just the right spaces for you. Enjoy a focused and relaxing Reddit experience, tailored to what truly matters in college life.</p>
-        <p>Here’s what you’ll find:</p>
+        <p class="nunito">Here’s what you’ll find:</p>
         <ul>
           <li><strong>r/college:</strong> Navigate college life with advice, tips, and shared experiences from fellow students.</li>
           <li><strong>r/StudyTips:</strong> Improve your study habits and academic performance with practical tips and strategies.</li>
           <li><strong>r/sunset:</strong> Take a moment to unwind with beautiful sunset photos, perfect for a relaxing break from your studies.
 </li>
           <li><strong>r/productivity:</strong> Get motivated with study tips, time management hacks, and more.</li>
-          <li><strong>r/CollegeHumor:</strong> Take a break and enjoy some light-hearted humor to brighten your day.</li>
+          <li><strong>r/CollegeMemes:</strong> Enjoy a collection of relatable and hilarious memes that capture the ups and downs of college life.</li>
         </ul>
-        <p>Welcome again to StudentThreads—your home for a better college experience.</p>
+        <p class="nunito">Welcome again to StudentThreads—your home for a better college experience.</p>
       </div>
     </div>
   );
